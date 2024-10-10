dbc0f2b44bead64b15e6ef9436e59e78dac85297
kowals14 <jakub.kowalski24@outlook.com>
2024-10-10 03:33:53 -0500

    check for multiple decimals, input character limit

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var b = 0;
 var operator = null;
 
 var newOperand = false;
+var decimal = false;
 
 const numpad = document.querySelectorAll(".row > div");
 const display = document.getElementById("textbox");
@@ -24,7 +25,6 @@ const operate = function() {
 }
 
 const readDisplay = function() {
-	console.log("Reading display: " + Number(display.innerHTML));
 	return Number(display.innerHTML);
 }
 
@@ -36,16 +36,18 @@ const appendDigit = function(digit) {
 	display.innerHTML = display.innerHTML + digit;
 }
 
-// this function updates the display with a new initial operand
-// and sets the necessary conditions to input a new operator and operand
+// this function updates the display with a new initial operand 'num'
+// and sets the necessary conditions to input the second operand
 const resetOperation = function(num) {
 	a = num;
 	updateDisplay(num);
 	operator = null;
 	newOperand = false;
+	decimal = false;
 	console.log("Resetting operation, initializing a with: " + a);
 }
 
+// assign the desired behavior to the corresponding key
 numpad.forEach((num) => {
 	num.addEventListener("mousedown", (event) => {
 		const key = event.target;
@@ -55,6 +57,8 @@ numpad.forEach((num) => {
 				resetOperation(0);
 				break;
 			case "=":
+				// check we have chosen an operator
+				// update the display to the result and reset to begin a new operation
 				if(operator) {
 					b = readDisplay();
 					// divide by zero check
@@ -76,21 +80,29 @@ numpad.forEach((num) => {
 			case "-":
 			case "*": 
 			case "/":
+				// when choosing the operator, set the first operand to the current 
+				// value on the display and mark that we are now inputting the second operand
 				a = readDisplay();
 				newOperand = true;
+				decimal = false;
 				operator = key.id;
 				break;
+			case ".":
+				// prevent adding more than one decimal point
+				if(!decimal) { decimal = true; }
+				else { break; }
 			default:
+				// check if the input is too big
 				if(readDisplay().toString().length < 15) {
+					// replace the number on the screen with a digit if the number is 0 or 
+					// we are creating a new operand 
 					if(newOperand || readDisplay() == 0) {
 						updateDisplay(key.id);
 						newOperand = false;
 					}
-					else {
-						appendDigit(key.id);
-					}
+					// otherwise we just append the digit to the end of the value on the display
+					else { appendDigit(key.id); }
 				}
-				break;
 		}	
 	});
 	num.addEventListener("mouseup", (event) => {
